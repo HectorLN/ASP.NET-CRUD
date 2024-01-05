6751b21acd5ee78563a5df061b6c08b6795f5ac3
PabloTriana <jose.triana18@tectijuana.edu.mx>
2024-01-05 15:29:20 -0800

    Funcionalidad boton delete

--- CRUDASP/Grid.aspx.cs
+++ CRUDASP/Grid.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +15,9 @@ namespace CRUDASP
 {
     public partial class Grid : System.Web.UI.Page
     {
+        //Agregar cadena de conexion
+        readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CRUDG3IConnectionString"].ConnectionString);
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["LoggedIn"] != null)
@@ -34,16 +40,49 @@ namespace CRUDASP
 
         protected void RadGrid1_ItemCommand(object sender, GridCommandEventArgs e)
         {
-            if (e.CommandName == "RowClick")
-            {
-                GridDataItem item = (GridDataItem)e.Item;
-                string nombre = item["Nombre"].Text;
-                string puesto = item["puesto"].Text;
-                string fecha = item["fecha_ingreso"].Text;
+            //if (e.CommandName == "RowClick")
+            //{
+            //    GridDataItem item = (GridDataItem)e.Item;
+            //    string nombre = item["Nombre"].Text;
+            //    string puesto = item["puesto"].Text;
+            //    string fecha = item["fecha_ingreso"].Text;
 
-                NombreLabel.Text = nombre;
-                PuestoLabel.Text = puesto;
-                FechaLabel.Text = "Empleado desde " + fecha;
+            //    NombreLabel.Text = nombre;
+            //    PuestoLabel.Text = puesto;
+            //    FechaLabel.Text = "Empleado desde " + fecha;
+            //}
+            //else if (e.CommandName == "Delete")
+            //{
+            //    // execute some logic
+            //    GridDataItem item = (GridDataItem)e.Item;
+            //    string id_empleado = item["id_empleado"].Text;
+
+            //    SqlCommand cmd = new SqlCommand("sp_delete_empleado", con);
+            //    con.Open();
+            //    cmd.CommandType = CommandType.StoredProcedure;
+            //    cmd.Parameters.Add("@Id_empleado", SqlDbType.Int).Value = int.Parse(id_empleado);
+            //    cmd.ExecuteNonQuery();
+            //    con.Close();
+
+            //}
+
+            if (e.CommandName == "MyCustomCommand1")
+            {
+                // execute some logic
+
+                GridDataItem item = (GridDataItem)e.Item;
+                string id_empleado = item["id_empleado"].Text;
+
+                SqlCommand cmd = new SqlCommand("sp_delete_empleado", con);
+                con.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Id_empleado", SqlDbType.Int).Value = int.Parse(id_empleado);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            else if (e.CommandName == "MyCustomCommand2")
+            {
+                // execute some logic
             }
         }
     }
