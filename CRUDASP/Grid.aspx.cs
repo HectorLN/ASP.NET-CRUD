1c209b58e56b7a2f481e29a7ca72e1589e02fe90
Favio Valdez <favioe1@outlook.com>
2024-01-05 17:45:53 -0700

    Cambios a funcionamiento; operaciones de CRUD casi completas

--- CRUDASP/Grid.aspx.cs
+++ CRUDASP/Grid.aspx.cs
@@ -10,6 +10,7 @@ using static System.EventArgs;
 using System.Web.UI.WebControls;
 using static System.Web.UI.WebControls.CommandEventArgs;
 using Telerik.Web.UI;
+using System.Collections;
 
 namespace CRUDASP
 {
@@ -84,6 +85,73 @@ namespace CRUDASP
             {
                 // execute some logic
             }
+        }
+
+        protected void RadGrid1_DeleteCommand(object sender, GridCommandEventArgs e)
+        {
+            GridDataItem item = (GridDataItem)e.Item;
+            string id_empleado = item["id_empleado"].Text;
+
+            SqlCommand cmd = new SqlCommand("sp_delete_empleado", con);
+            con.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Id_empleado", SqlDbType.Int).Value = int.Parse(id_empleado);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_InsertCommand(object sender, GridCommandEventArgs e)
+        {
+            Hashtable values = new Hashtable();
+            var editableItem = ((GridEditableItem)e.Item);
+            editableItem.ExtractValues(values);
+
+
+            SqlCommand cmd2 = new SqlCommand("sp_comparacion_puesto", con);
+            con.Open();
+            cmd2.CommandType = CommandType.StoredProcedure;
+            cmd2.Parameters.Add("@Puesto", SqlDbType.VarChar).Value = values["puesto"];
+            //SqlParameter retval = new SqlParameter("@ReturnValue", System.Data.SqlDbType.Int);
+            //retval.Direction = System.Data.ParameterDirection.ReturnValue;
+            //cmd2.Parameters.Add(retval);
+            //var outputParamter = cmd2.Parameters.Add("@Return", SqlDbType.Int);
+            //outputParamter.Direction = ParameterDirection.ReturnValue;
+            cmd2.ExecuteNonQuery();
+            //var resultado = (int)cmd2.Parameters["@RETURN_VALUE"].Value;
+            var resultado = 2;
+
+            //
+
+            SqlCommand cmd = new SqlCommand("sp_create_empleado", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = values["nombre"];
+            cmd.Parameters.Add("@Id_puesto", SqlDbType.Int).Value = resultado;
+            cmd.Parameters.Add("@Foto", SqlDbType.VarChar).Value = " ";
+            cmd.Parameters.Add("@Fecha", SqlDbType.Date).Value = values["fecha_ingreso"];
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_UpdateCommand(object sender, GridCommandEventArgs e)
+        {
+            Hashtable values = new Hashtable();
+            var editableItem = ((GridEditableItem)e.Item);
+            editableItem.ExtractValues(values);
+
+            var id_empleado = (int)editableItem.GetDataKeyValue("id_empleado");
+
+            SqlCommand cmd = new SqlCommand("sp_update", con);
+            con.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Id_empleado", SqlDbType.Int).Value = id_empleado;
+            cmd.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = values["nombre"];
+            cmd.Parameters.Add("@Id_puesto", SqlDbType.Int).Value = 2;
+            cmd.Parameters.Add("@Foto", SqlDbType.VarChar).Value = " ";
+            cmd.Parameters.Add("@Fecha", SqlDbType.Date).Value = values["fecha_ingreso"];
+
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
